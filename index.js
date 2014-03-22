bb0f70efb5e50d1f9e3a5536847750ccd4336379
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2014-03-22 00:39:53 -0400

    Switch out pagemaki dependency expectation

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
+var Pagemaki = require("pagemaki");
 var Buffer = require("buffer").Buffer;
 var through = require("through");
 
 /**
- * Return a file handling function that can 
+ * Returns a file handling function that can 
  * be piped to from within a gulp workflow
  * 
  * @return {[type]} [description]
  */
-Pagemaki.prototype.stream = function () {
+module.exports = function (options) {
 
-  var self = this;
+  var maker = new Pagemaki(options);
 
   function write(file) {
 
     var stream = this;
 
-    self.make(file._contents.toString(), function (err, made) {
+    maker.make(file._contents.toString(), function (err, made) {
       file._contents = new Buffer(made);
       stream.queue(file);
     });
